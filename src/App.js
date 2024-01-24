05e0d273517014603c07c45cef93c10d7b32c495
Cuong Doan <cuong.doan@asnet.com.vn>
2024-01-24 11:23:36 +0700

    Add prismic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  PrismicRichText,
+  useFirstPrismicDocument,
+  PrismicImage,
+  // useAllPrismicDocumentsByType,
+  // usePrismicDocumentByUID,
+  useSinglePrismicDocument,
+} from '@prismicio/react';
+import * as prismic from '@prismicio/client'
 
 function App() {
+  const [document] = useFirstPrismicDocument()
+  console.log(document);
+  const test_single = useSinglePrismicDocument("test_single");
+  console.log(test_single);
+  // const [documents] = useAllPrismicDocumentsByType('article')
+  // const [page, { state }] = usePrismicDocumentByUID('page', 'hello-world')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {document && (
+        <div>
+          <PrismicImage field={document.data.body[0].primary.test_image} />
+          <time
+            dateTime={prismic.asDate(document.data.body[1].items[0].test_timestamp).toISOString()}
+          >
+            {prismic.asDate(document.data.body[1].items[0].test_timestamp).toISOString()}
+          </time>
+        </div>
+      )}
     </div>
   );
 }
